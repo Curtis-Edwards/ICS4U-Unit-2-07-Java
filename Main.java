69c8a1b4a2552efdd8e5648cd038b4e15e535b9f
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-18 17:08:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -27,38 +27,44 @@ final class Main {
     * @param args No args will be used
     */
     public static void main(String[] args) {
-        Bike bmx = new Bike("Red", 40);
+        // Constants for magic numbers
+        final int tenNumber = 10;
+        final int fortyNumber = 40;
+        final int fifteenNumber = 15;
+        final int twoHundredNumber = 200;
+
+        Bike bmx = new Bike("Red", fortyNumber);
 
         System.out.println("Created Bmx bike.\nStatus:\n");
         bmx.status();
 
         System.out.println("Set the cadence to 10\n");
-        bmx.setCadence(10);
+        bmx.setCadence(tenNumber);
         bmx.status();
 
         System.out.println("\nAccelerate by 15:");
-        bmx.accelerate(15);
+        bmx.accelerate(fifteenNumber);
         bmx.status();
 
         System.out.println("\nRing bell.");
         bmx.ringBell();
 
 
-        Truck bigTruck = new Truck("Grey", 200, "HGC-3456F");
+        Truck bigTruck = new Truck("Grey", twoHundredNumber, "HGC-3456F");
 
         System.out.println("Created a Truck.\nStatus:\n");
         bigTruck.status();
 
         System.out.println("\nAccelerating, 10 of power for ten seconds:");
-        bigTruck.accelerate(10, 10);
+        bigTruck.accelerate(tenNumber, tenNumber);
         System.out.println("New speed: " + bigTruck.getSpeed());
 
         System.out.println("\nBreaking, 10 of power for 10 sec.");
-        bigTruck.accelerate(10, 10);
+        bigTruck.accelerate(tenNumber, tenNumber);
         System.out.println("New speed: " + bigTruck.getSpeed());
 
         System.out.println("\nApplied air pressure of 10:");
-        bigTruck.applyAir(10);
+        bigTruck.applyAir(tenNumber);
         System.out.println("New speed: " + bigTruck.getSpeed());
 
         System.out.println("\nDone.");
